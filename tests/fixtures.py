f0c6bebe2cac7eafe1a90b61cd54cde468dc6e9d
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2024-01-16 10:52:28 +0100

    Add CLI tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,9 +1,35 @@
 from pathlib import Path
 from pytest import fixture
 
+VALID_SAMPLESHEET_STR = """
+[Header]
+IEMFileVersion,4
+Institute,NUS
+Instrument,NextSeq
+[Settings]
+Adapter,CTGTCTCTTATACACATCT
+[Reads]
+151
+151
+[Data]
+Sample_ID,Sample_Name
+S1,Sample1
+S2,Sample2
+S3,Sample3
+S4,Sample4
+"""
+
 
 @fixture()
-def empty_samplesheet(tmpdir):
-    samplesheet = Path(tmpdir) / "samplesheet.csv"
+def empty_samplesheet_path(tmpdir):
+    samplesheet = Path(tmpdir) / "empty_samplesheet.csv"
     samplesheet.touch()
+    return samplesheet
+
+
+@fixture()
+def valid_samplesheet_path(tmpdir):
+    samplesheet = Path(tmpdir) / "valid_samplesheet.csv"
+    with samplesheet.open("w") as f:
+        f.write(VALID_SAMPLESHEET_STR)
     return samplesheet
